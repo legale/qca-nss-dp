43afb9b7f615a912c74d17624f9024458a6514ed
Parikshit Gune <quic_pgune@quicinc.com>
2023-07-18 12:07:50 +0530

    [qca-nss-dp] Support Core selection for PPE mirrored packets.
    Change-Id: I0df934a752c1913ddd24cefed4b909adc8abfbaa
    Signed-off-by: Parikshit Gune <quic_pgune@quicinc.com>

--- hal/dp_ops/edma_dp/edma_v2/edma_cfg_rx.c
+++ hal/dp_ops/edma_dp/edma_v2/edma_cfg_rx.c
@@ -780,6 +780,35 @@ static void edma_cfg_rx_point_offload_rings_to_rx_fill_mapping(struct edma_gbl_c
 #endif
 
 /*
+ * edma_cfg_rx_mcast_qid_to_core_mapping
+ *	Configure mcast queue to core mapping.
+ */
+void edma_cfg_rx_mcast_qid_to_core_mapping(struct edma_gbl_ctx *egc, uint8_t core_id)
+{
+	uint32_t desc_index, q_id;
+	uint32_t reg_index, data;
+
+	/*
+	 * Map PPE multicast queues to the Rx ring according to the core.
+	 */
+	desc_index = egc->rxdesc_ring_map[0][core_id];
+	for (q_id = EDMA_CPU_PORT_MCAST_QUEUE_START;
+		q_id <= EDMA_CPU_PORT_MCAST_QUEUE_END;
+			q_id += EDMA_QID2RID_NUM_PER_REG) {
+		reg_index = q_id/EDMA_QID2RID_NUM_PER_REG;
+		data = EDMA_RX_RING_ID_QUEUE0_SET(desc_index) |
+			EDMA_RX_RING_ID_QUEUE1_SET(desc_index) |
+			EDMA_RX_RING_ID_QUEUE2_SET(desc_index) |
+			EDMA_RX_RING_ID_QUEUE3_SET(desc_index);
+
+		edma_reg_write(EDMA_QID2RID_TABLE_MEM(reg_index), data);
+
+		edma_info("Configure QID2RID(%d) reg:0x%x to 0x%x\n",
+				q_id, EDMA_QID2RID_TABLE_MEM(reg_index), data);
+	}
+}
+
+/*
  * edma_cfg_rx_qid_to_rx_desc_ring_mapping()
  *	Configure PPE queue id to Rx ring mapping
  */
--- hal/dp_ops/edma_dp/edma_v2/edma_cfg_rx.h
+++ hal/dp_ops/edma_dp/edma_v2/edma_cfg_rx.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
  *
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -60,6 +60,7 @@ void edma_cfg_rx_napi_enable(struct edma_gbl_ctx *egc);
 void edma_cfg_rx_napi_delete(struct edma_gbl_ctx *egc);
 void edma_cfg_rx_napi_add(struct edma_gbl_ctx *egc, struct net_device *netdev);
 void edma_cfg_rx_mapping(struct edma_gbl_ctx *egc);
+void edma_cfg_rx_mcast_qid_to_core_mapping(struct edma_gbl_ctx *egc, uint8_t core_id);
 void edma_cfg_rx_rings_enable(struct edma_gbl_ctx *egc);
 void edma_cfg_rx_rings_disable(struct edma_gbl_ctx *egc);
 int edma_cfg_rx_fc_enable_handler(struct ctl_table *table, int write,
