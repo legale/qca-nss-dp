bc55f7552ca861a0f4873e51b01750fabbbdc5fc
Swati Singh <quic_swasing@quicinc.com>
2023-10-12 16:11:21 +0530

    [qca-nss-dp]: Enable legacy SCS for PPE-VP.
    For legacy SCS rule type, if wifi_qos_flag is set as enable then
    edma rx handle will populate skb->mark with valid SAWF tag
    and flow_qos_value.
    
    Change-Id: Ieb50dd172e3f712a8eebbe4333e42f6cff0fd046
    Signed-off-by: Swati Singh <quic_swasing@quicinc.com>

--- hal/dp_ops/edma_dp/edma_v2/edma_rx.c
+++ hal/dp_ops/edma_dp/edma_v2/edma_rx.c
@@ -327,14 +327,28 @@ static void edma_rx_handle_wifi_qos_packets(struct edma_gbl_ctx *egc, struct edm
 		/*
 		 * Update stats for the SAWF service class.
 		 */
+		BUG_ON(!PPE_DRV_SERVICE_CLASS_IS_VALID(service_class));
 		edma_rx_sawf_sc_stats_update(skb->len, &egc->sawf_sc_stats[service_class]);
-
 		/*
 		 * Configure skb->mark with SAWF metadata.
 		 */
 		skb->mark = EDMA_RX_SAWF_METADATA_CONSTRUCT(service_class, peer_id, wifi_qos);
 
 		edma_debug("%px : SAWF mark configured = 0x%x\n", egc, skb->mark);
+		break;
+
+	case PPE_DRV_TREE_ID_TYPE_SCS:
+		/*
+		 * In case of SCS, fetch the wifi_qos from Tree ID.
+		 */
+		wifi_qos = EDMA_RXDESC_WIFI_QOS_GET(rxdesc_head);
+
+		/*
+		 * Configure skb->mark with wifi_qos metadata.
+		 */
+		skb->mark = wifi_qos;
+
+		edma_debug("%px : SCS mark configured = 0x%x\n", egc, skb->mark);
 		break;
 
 	case PPE_DRV_TREE_ID_TYPE_WIFI_TID:
